4fcb9b1f4206053f561a3f134345fafed46b0ec9
unknown <montherismail90@gmail.com>
2022-03-14 13:29:26 +0200

    create routes protector functionality Relates #41

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,10 @@
 const signUp = require('./signUp');
 const login = require('./login');
+const { routesProtector } = require('./routesProtector');
 const { renderPostsPub } = require('./renderPostsPub');
 const { addPost } = require('./addPost');
 const { upVote } = require('./upVote');
 const { downVote } = require('./downVote');
 const { renderPostsHome } = require('./renderPostsHome');
 
-module.exports = { signUp, login, renderPostsPub, addPost, upVote, downVote, renderPostsHome };
+module.exports = { signUp, login, routesProtector, renderPostsPub, addPost, upVote, downVote, renderPostsHome };
